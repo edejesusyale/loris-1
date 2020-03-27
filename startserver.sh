776a984005463bde6c24e9e4c077222b8e9a0707
Eric DeJesus <eric.dejesus@yale.edu>
2020-03-27 13:25:17 -0400

    All \ are replaced with space now

--- startserver.sh
+++ startserver.sh
@@ -6,7 +6,7 @@ read -p 'accesskey: ' accesskey
 read -p 'secretkey: ' secretkey
 echo
 
-sed -i  "s user_access_key ${accesskey}/g" ${dirname}/loris/s3resolver.py
-sed -i  "s user_secret_key ${secretkey}/g" ${dirname}/loris/s3resolver.py
+sed -i  "s user_access_key ${accesskey} g" ${dirname}/loris/s3resolver.py
+sed -i  "s user_secret_key ${secretkey} g" ${dirname}/loris/s3resolver.py
 
 python3.6 ${dirname}/loris/webapp.py
