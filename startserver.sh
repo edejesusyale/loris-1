3b0e82a3bf6e02155af2ca3b0581cd0ebb115dbd
Eric DeJesus <eric.dejesus@yale.edu>
2020-03-27 11:15:20 -0400

    Switched replace command

--- startserver.sh
+++ startserver.sh
@@ -5,6 +5,8 @@ dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 read -p 'accesskey: ' accesskey
 read -p 'secretkey: ' secretkey
 echo
-sed -i  "s/user_access_key/${accesskey}/g" ${dirname}/loris/s3resolver.py
-sed -i  "s/user_secret_key/${secretkey}/g" ${dirname}/loris/s3resolver.py
+
+awk '{gsub("user_secret_key_key", "${secretkey}", $0); print}'
+awk '{gsub("user_access_key", "${accesskey}", $0); print}' ${dirname}/loris/s3resolver.py
+awk '{gsub("user_secret_key_key", "${secretkey}", $0); print}' ${dirname}/loris/s3resolver.py
 python3.6 ${dirname}/loris/webapp.py
