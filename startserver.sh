7c954e1e49cf96b990a2abc165fa5e82f0ff1356
Eric DeJesus <eric.dejesus@yale.edu>
2020-03-27 11:28:13 -0400

    Fixed it

--- startserver.sh
+++ startserver.sh
@@ -6,7 +6,6 @@ read -p 'accesskey: ' accesskey
 read -p 'secretkey: ' secretkey
 echo
 
-awk '{gsub("user_secret_key_key", "${secretkey}", $0); print}'
 awk '{gsub("user_access_key", "${accesskey}", $0); print}' ${dirname}/loris/s3resolver.py
 awk '{gsub("user_secret_key_key", "${secretkey}", $0); print}' ${dirname}/loris/s3resolver.py
 python3.6 ${dirname}/loris/webapp.py
